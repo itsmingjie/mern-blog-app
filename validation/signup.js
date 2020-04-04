b8e1062d1787d62ba3897345afa9b5de775605b3
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2020-04-04 23:28:41 +0700

    membuat api validation untul login, signup, dan post

--- validation/signup.js
+++ validation/signup.js
@@ -1,35 +1,35 @@
 //for validation signup
 const Validator = require("validator");
-cont isEmpty = require("is-empty");
+const isEmpty = require("is-empty");
 
 module.exorts = validateSignUpInput = data => {
   let errors = {};
 
-  let {user_name, email, password} = data;
+  let { user_name, email, password } = data;
 
   //converting empty fields to empty string as validator function works only with strings
   user_name = !isEmpty(user_name) ? user_name : "";
   email = !isEmpty(email) ? email : "";
   password = !isEmpty(password) ? password : "";
 
-  if(Validator.isEmpty(user_name)){
+  if (Validator.isEmpty(user_name)) {
     errors.user_name = "Username is required";
   }
 
-  if(Validator.isEmpty(email)) {
-    errors.email = "Email is required"
+  if (Validator.isEmpty(email)) {
+    errors.email = "Email is required";
   } else if (!Validator.isEmail(email)) {
-    errors.email = "Enter a valid email id"
+    errors.email = "Enter a valid email id";
   }
 
   if (Validator.isEmpty(password)) {
     errors.password = "Password is required";
-  } else if (!Validator.isLength(password, {min: 6, max: 30})) {
-    errors.password = "Password must be at least 6 character"
+  } else if (!Validator.isLength(password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 character";
   }
 
   return {
     errors,
     isValid: isEmpty(errors)
-  }
-}
+  };
+};
