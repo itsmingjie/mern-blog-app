2c139059e42205efda82d73ca615f27cd7f1afa5
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-04-04 23:04:22 +0700

    membuat schema untuk mongoDB (#4)
    Co-authored-by: irwantowibowo1992 <irwantowibowo1992@hotmail.com>

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,26 @@
 // for post model
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const post = {
+  titile: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+};
+
+const PostSchema = new Schema(post);
+
+module.exports = mongoose.model("posts", PostSchema);
