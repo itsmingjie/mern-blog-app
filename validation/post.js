783ebfee2f2ec45b78cb27442c442e3af5bf572c
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2020-04-04 23:27:25 +0700

    membuat validation api untuk login, signup, dan post

--- validation/post.js
+++ validation/post.js
@@ -1,1 +1,26 @@
 //validation post
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+
+module.exports = validatePostInput = data => {
+  let errors = {};
+
+  let { title, body } = data;
+
+  //converting empty fields to empty string as validator function works only with strings
+  title = !isEmpty(title) ? title : "";
+  bosy = !isEmpty(body) ? body : "";
+
+  if (Validator.isEmpty(title)) {
+    errors.title = "Title is required";
+  }
+
+  if (Validator.isEmpty(body)) {
+    errors.body = "Description is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
