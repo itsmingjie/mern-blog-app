dbf7b4d3338632d5343cf6785bc25bffe3b5bc87
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2020-04-04 23:03:30 +0700

    membuat schema untuk mongoDB

--- models/User.js
+++ models/User.js
@@ -1,1 +1,21 @@
 // for user model
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userModel = {
+  user_name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+};
+
+const UserSchema = new Schema(UserSchema);
+module.exports = mongoose.model("user", UserSchema);
