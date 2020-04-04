5e9acee090939bbf14dcea3a686a1d4bd3fe0f8c
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-04-04 22:51:09 +0700

    koneksi ke mongoDB dengan mongoose (#3)
    Co-authored-by: irwantowibowo1992 <irwantowibowo1992@hotmail.com>

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require("express");
+require("dotenv").config(); //untuk load environment variabel
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("Hello backend, siap berpetualang??");
-});
+const MONGO_URI = process.env.MONGO_URI;
 
-app.post("/user", (req, res) => {
-  console.log("req.body");
-  res.send(req.body);
-});
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Koneksi ke mongoDB berhasil"))
+  .catch(err => console.log("error tuh"));
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
 });
